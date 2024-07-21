5a80b88b02a766f806b3f5e96b1d14a6bc8c3aaa
trobonox <57040351+trobonox@users.noreply.github.com>
2024-07-21 13:25:47 +0200

    feat: add general content structure

--- app/source.ts
+++ app/source.ts
@@ -1,9 +1,20 @@
 import { map } from '@/.map';
 import { createMDXSource } from 'fumadocs-mdx';
 import { loader } from 'fumadocs-core/source';
+import { icons } from 'lucide-react';
+import { createElement } from 'react';
+import { create } from '@/components/icon';
 
 export const { getPage, getPages, pageTree } = loader({
   baseUrl: '/docs',
   rootDir: 'docs',
   source: createMDXSource(map),
+  icon(icon) {
+    if (!icon) {
+        // You may set a default icon
+        return;
+    }
+
+    if (icon in icons) return create({ icon: icons[icon as keyof typeof icons] });
+  },
 });
